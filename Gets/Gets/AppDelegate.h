a9a9000bf68a2583aaa69db1c30206df2336f662
Chimello44 <hugoluizchimello@gmail.com>
2015-03-02 14:16:08 -0300

    First Commit // simple storyboard

---
+++ Gets/Gets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gets
+//
+//  Created by Hugo Luiz Chimello on 3/2/15.
+//  Copyright (c) 2015 Hugo Luiz Chimello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gets/Gets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gets
+//
+//  Created by Hugo Luiz Chimello on 3/2/15.
+//  Copyright (c) 2015 Hugo Luiz Chimello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
